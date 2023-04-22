17ed8ae0835146f5f53831fd8b8922ed7ec1537c
Jacob Leisten <leistejb@mail.uc.edu>
2023-04-22 18:14:19 -0400

    Scaffold the Database

---
+++ DotNetCoreSqlDb/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreSqlDb.Models;
+
+public partial class Product
+{
+    public int ProductNum { get; set; }
+
+    public string Department { get; set; }
+
+    public string Commodity { get; set; }
+
+    public string BrandTy { get; set; }
+
+    public string NaturalOrganicFlag { get; set; }
+}
