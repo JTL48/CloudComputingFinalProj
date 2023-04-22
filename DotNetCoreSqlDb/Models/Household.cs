0189e8854d35d3dbd6fe0ee02944fbf48d44eb98
Jacob Leisten <leistejb@mail.uc.edu>
2023-04-22 19:49:27 -0400

    Scaffold Fixes

---
+++ DotNetCoreSqlDb/Models/Household.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreSqlDb.Models
+{
+    public partial class Household
+    {
+        public short HshdNum { get; set; }
+        public string L { get; set; }
+        public string AgeRange { get; set; }
+        public string Marital { get; set; }
+        public string IncomeRange { get; set; }
+        public string Homeowner { get; set; }
+        public string HshdComposition { get; set; }
+        public string HhSize { get; set; }
+        public string Children { get; set; }
+    }
+}
---
+++ DotNetCoreSqlDb/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreSqlDb.Models
+{
+    public partial class Transaction
+    {
+        public int BasketNum { get; set; }
+        public short HshdNum { get; set; }
+        public DateTime Purchase { get; set; }
+        public int ProductNum { get; set; }
+        public double Spend { get; set; }
+        public float Units { get; set; }
+        public string StoreR { get; set; }
+        public byte WeekNum { get; set; }
+        public short Year { get; set; }
+    }
+}
