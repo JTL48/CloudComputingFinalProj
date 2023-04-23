fee7b388d45df4120bd01e442ffeb679aa1248d4
Jacob Leisten <leistejb@mail.uc.edu>
2023-04-22 20:17:30 -0400

    HouseholdOverview Start

---
+++ DotNetCoreSqlDb/Models/View/HouseholdOverviewView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace DotNetCoreSqlDb.Models.View
+{
+    public class HouseholdOverviewView
+    {
+        public IEnumerable<Household> Households { get; set; }
+        public IEnumerable<Product> Products{ get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; }
+    }
+}
